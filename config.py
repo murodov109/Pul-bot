a450e3a444b010197e20b859495d4fea2ca42e41
murodov109 <akabekov77@gmail.com>
2025-11-17 18:16:07 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("8236617508:AAHf2qGfsd7xBmjj3WipAivPssOtjd5Cs-s")
 ADMIN_IDS = [int(x) for x in os.getenv("5253025422", "").split(",") if x.strip()]
 PROOF_CHANNEL_ID = int(os.getenv("PROOF_CHANNEL_ID", "0"))
-MANDATORY_CHANNEL_IDS = [int(x) for x in os.getenv("MANDATORY_CHANNEL_IDS", "").split(",") if x.strip()]
+MANDATORY_CHANNEL_IDS = [int(x) for x in os.getenv("-1003334917965", "").split(",") if x.strip()]
 DATABASE_URL = os.getenv("DATABASE_URL")
 MIN_WITHDRAW = 10000
 DAILY_WITHDRAW_LIMIT = int(os.getenv("DAILY_WITHDRAW_LIMIT", "0"))
