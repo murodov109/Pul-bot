edfa7eaca8c75522788c759a850c15edb973b00c
murodov109 <akabekov77@gmail.com>
2025-11-17 17:55:36 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("8236617508:AAHf2qGfsd7xBmjj3WipAivPssOtjd5Cs-s")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()]
+ADMIN_IDS = [int(x) for x in os.getenv("5253025422", "").split(",") if x.strip()]
 PROOF_CHANNEL_ID = int(os.getenv("PROOF_CHANNEL_ID", "0"))
 MANDATORY_CHANNEL_IDS = [int(x) for x in os.getenv("MANDATORY_CHANNEL_IDS", "").split(",") if x.strip()]
 DATABASE_URL = os.getenv("DATABASE_URL")
